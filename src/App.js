18fa4ffe8efa9eaa68c115e74d95d236fbac9d62
Connor McCue <connor.mccue@comcast.net>
2022-10-03 00:43:29 -0700

    better version of console.log(users.json)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { Link, Route, Switch } from 'react-router-dom';
-import Table from './components/Table.js';
+import Table from './Table.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/table" component={Table}>Table</Link>
+        <Link to="/table">Table</Link>
         <Link to="/charts">Charts</Link>
       </nav>
       <Switch>
         <Route exact path="/table">
           <h1>Table Libary</h1>
+          <Table />
         </Route>
         <Route path="/charts">
           <h1>Chart Library</h1>
--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import usersData from '../users.json';
+import usersData from './users.json';
 
 export default function Table() {
   const [users, setUsers] = useState(usersData);
