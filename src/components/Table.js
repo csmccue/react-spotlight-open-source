6de4b656820c1aeba78e8c713b2fed9a9c74e417
Connor McCue <connor.mccue@comcast.net>
2022-10-03 00:31:12 -0700

    testing example code

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
+import usersData from '../users.json';
 
 export default function Table() {
+  const [users, setUsers] = useState(usersData);
+  console.log(users);
   return (
     <div>Table</div>
   );
