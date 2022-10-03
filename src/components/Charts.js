b42cd25621778f814e103a6e59145c3e654be15a
Connor McCue <connor.mccue@comcast.net>
2022-10-03 00:29:19 -0700

    react chartjs install, testing example from chartjs github

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
+import { Doughnut } from 'react-chartjs-2';
 
 export default function Charts() {
+  const [donuts, useDonuts] = useState()
+  console.log(Doughnut);
   return (
-    <div>Charts</div>
+    <>
+        <div>Charts</div>
+    <Doughnut data={...} />
+    </>
+
   );
 }
